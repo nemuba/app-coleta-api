5e40df4d60a7d9eb5c5854a940dadec99f8c7733
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-06-25 15:32:32 -0300

    run rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_190301) do
+ActiveRecord::Schema.define(version: 2021_06_25_182003) do
 
   create_table "addresses", charset: "utf8mb4", force: :cascade do |t|
     t.string "street"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 2021_06_24_190301) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_collects_on_user_id"
+  end
+
+  create_table "item_types", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", charset: "utf8mb4", force: :cascade do |t|
