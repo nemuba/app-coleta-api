2ef4057c1832427b0a2048a095922be3f825a430
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-06-25 08:01:52 -0300

    fix documentation

--- app/models/swagger/user.rb
+++ app/models/swagger/user.rb
@@ -21,6 +21,31 @@ module Swagger
         key :'$ref', :ProfileDTO
       end
 
+      property :user_point do
+        key :'$ref', :UserPointDTO
+      end
+
+      property :collects do
+        key :type, :array
+        items do
+          key :'$ref', :CollectDTO
+        end
+      end
+
+      property :routes do
+        key :type, :array
+        items do
+          key :'$ref', :RouteDTO
+        end
+      end
+
+      property :user_modules do
+        key :type, :array
+        items do
+          key :'$ref', :SystemModuleDTO
+        end
+      end
+
       property :created_at do
         key :type, :string
         key :format, "yyyy-MM-dd HH24:MI:SS.MS Z"
