7def2ecea821beda0073fbe4fc45d37a9c6f1b12
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-07-20 13:49:15 -0300

    fix: error in serializer item_collect_serializer

--- app/serializers/item_collect_serializer.rb
+++ app/serializers/item_collect_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ItemCollectSerializer < ActiveModel::Serializer
-  attribute :id, item_id, :collect_id, :created_at, :updated_at
+  attribute :id, :item_id, :collect_id, :created_at, :updated_at
 
   has_one :collect
 end
