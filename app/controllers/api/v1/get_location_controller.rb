3f3b2d2f9e0aa70be4c3d337d5fb572823261fac
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-07-29 09:00:34 -0300

    fix: change controller get_location

--- app/controllers/api/v1/get_location_controller.rb
+++ app/controllers/api/v1/get_location_controller.rb
@@ -2,16 +2,24 @@ module Api
   module V1
     class GetLocationController < ApplicationController
       before_action :authenticate_and_set_user
-      
+
       def create
-        
+        return render json: {
+          status: "error",
+          msg: [
+            "Parâmetros inválidos! Parâmetros necessários: longitude, latitude"
+          ]
+        }, status: :unprocessable_entity unless params_valid?
+
+        data = GetLocation.call(params[:latitude], params[:longitude])
+
+        json_response(data)
       end
 
       private
-
-      def location_params
-        params.require(:coordinates).permit(:latitude, :longitude)
-      end
+        def params_valid?
+          params[:latitude].present? && params[:longitude].present?
+        end
     end
   end
 end
