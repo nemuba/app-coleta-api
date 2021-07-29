a18e655b1d91fa77a45720d963666b9c1414da62
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-07-29 09:25:30 -0300

    fix: erros

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,10 +3,19 @@
 class Address < ApplicationRecord
   belongs_to :profile
 
-  validates :street, :neighborhood, :number, :city, :state, :country, :zip_code, presence: true
+  validates :street,
+            :neighborhood,
+            :number,
+            :city,
+            :state,
+            :country,
+            :zip_code,
+            :latitude,
+            :longitude,
+            presence: true
 
   geocoded_by :address
-  after_validation :geocode
+  after_initialize :geocode
 
   def address
     [number, street, neighborhood, city, state].compact.join(", ")
