79b9c8cab7386233ff694831c0cb62fc60e3ba5c
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-07-29 21:01:11 -0300

    fix rubocop

--- app/serializers/base_serializer.rb
+++ app/serializers/base_serializer.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class BaseSerializer < ActiveModel::Serializer
   attributes :created_at, :updated_at
-  
+
   def created_at
     object.created_at.strftime("%d-%m-%Y")
   end
