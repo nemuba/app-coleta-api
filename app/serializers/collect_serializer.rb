1002c138d99ffedd4573dbb869ace91f93ef444a
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-08-12 17:07:15 -0300

    fix: change created_at in collects_serializer

--- app/serializers/collect_serializer.rb
+++ app/serializers/collect_serializer.rb
@@ -2,7 +2,11 @@
 
 class CollectSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :note, :created_at, :updated_at
-  
+
   has_one :user
   has_one :collect_status
+
+  def created_at
+    object.created_at.strftime("%d/%m/%Y")
+  end
 end
