f8940dec766360d7c2d612e7962303d7275971bf
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-08-12 16:55:25 -0300

    fix: add attribute user_id in collects_serializer

--- app/serializers/collect_serializer.rb
+++ app/serializers/collect_serializer.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class CollectSerializer < ActiveModel::Serializer
-  attributes :id, :note, :created_at, :updated_at
+  attributes :id, :user_id, :note, :created_at, :updated_at
+  
   has_one :user
   has_one :collect_status
 end
