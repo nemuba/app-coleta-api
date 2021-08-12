e604f7106962087fb5a8613fe1aa6dd4b2ca35e5
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-08-12 19:28:50 -0300

    fix: add user_id in profile_serializer

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :name, :phone, :email, :document, :created_at, :updated_at
+  attributes :id, :user_id, :name, :phone, :email, :document
   belongs_to :user
   has_one :address
 end
