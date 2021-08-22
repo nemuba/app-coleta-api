e69fcb918092cce85345eeeb6b778a1685507242
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-08-22 20:16:17 -0300

    fix: route model

--- app/models/route.rb
+++ app/models/route.rb
@@ -6,11 +6,18 @@ class Route < ApplicationRecord
   has_many :route_collects
   accepts_nested_attributes_for :route_collects, allow_destroy: true
 
-  after_save :updated_collects_status
+  after_save :update_collect_status
+  before_destroy :remove_collect_status
 
-  def updated_collects_status
+  def update_collect_status
     route_collects.each do |rc|
       rc.collect.update(collect_status_attributes: { id: rc.collect.collect_status.id, name: "Aguardando Coleta" })
+    end
+  end
+
+  def remove_collect_status
+    route_collects.each do |rc|
+      rc.collect.update(collect_status_attributes: { id: rc.collect.collect_status.id, name: "Aguardando Confirmação" })
     end
   end
 end
