85b8ae0fbc225737e269ded43ae4943896f7c032
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-08-22 19:07:32 -0300

    fix: route controller

--- app/controllers/api/v1/collect/routes_controller.rb
+++ app/controllers/api/v1/collect/routes_controller.rb
@@ -8,16 +8,18 @@ module Api
         before_action :set_route, only: [:show, :update, :destroy]
         load_and_authorize_resource
 
+        INCLUDES = ["user", "route_collects", "route_collects.collect"]
+
         # GET /routes
         def index
           routes = Route.all
 
-          json_response(routes)
+          render json: routes, include: INCLUDES
         end
 
         # GET /routes/1
         def show
-          json_response(@route)
+          render json: @route, include: INCLUDES
         end
 
         # POST /routes
@@ -25,18 +27,18 @@ module Api
           route = Route.new(route_params)
 
           if route.save
-            json_response(route, :created)
+            render json: route, status: :created, include: INCLUDES
           else
-            json_response_error(route.errors)
+            render json: route.errors, status: :unprocessable_entity, include: INCLUDES
           end
         end
 
         # PATCH/PUT /routes/1
         def update
           if @route.update(route_params)
-            json_response(@route)
+            render json: @route, status: :created, include: INCLUDES
           else
-            json_response_error(@route.errors)
+            render json: @route.errors, status: :unprocessable_entity, include: INCLUDES
           end
         end
 
