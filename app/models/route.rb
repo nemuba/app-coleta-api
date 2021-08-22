eca5eba64102090ace611cb63cb7665f74282c2e
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-08-22 19:58:49 -0300

    fix: route controller

--- app/models/route.rb
+++ app/models/route.rb
@@ -5,4 +5,12 @@ class Route < ApplicationRecord
 
   has_many :route_collects
   accepts_nested_attributes_for :route_collects, allow_destroy: true
+
+  after_save :updated_collects_status
+
+  def updated_collects_status
+    route_collects.each do |rc|
+      rc.collect.update(collect_status_attributes: { id: rc.collect.collect_status.id, name: "Aguardando Coleta" })
+    end
+  end
 end
