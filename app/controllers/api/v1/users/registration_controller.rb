b6a06501a230c8bb98a8d1dc38726510ddbca008
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-08-25 16:05:45 -0300

    feat: add user_point

--- app/controllers/api/v1/users/registration_controller.rb
+++ app/controllers/api/v1/users/registration_controller.rb
@@ -5,9 +5,11 @@ module Api
     module Users
       class RegistrationController < ApiGuard::RegistrationController
         before_action :authenticate_resource, only: [:destroy]
-
+        after_action :set_user_point, only: [:create]
+        
         def create
-          init_resource(sign_up_params)
+          user_params = sign_up_params
+          init_resource(user_params.except(:token))
           if resource.save
             create_token_and_set_header(resource, resource_name)
             # render_success(message: I18n.t("api_guard.registration.signed_up"))
@@ -24,7 +26,21 @@ module Api
 
         private
           def sign_up_params
-            params.require(:auth).permit(:email, :password, :password_confirmation, :role)
+            params.require(:auth).permit(:email, :password, :password_confirmation, :role, :token)
+          end
+
+          def set_user_point
+            return unless params[:token].present?
+
+            self.resource = resource_class.find_by(token: params[:token])
+            return unless resource
+
+            unless resource.user_point.present?
+              resource.update!(user_point_attributes: { value: 10 })
+            else
+              points = resource.user_point
+              resource.update!(user_point_attributes: { id: points.id, value: points.value + 10 })
+            end
           end
       end
     end
