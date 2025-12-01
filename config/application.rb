9b08ce9758cbe2fa2aeaae7858e16083f6ad7903
Alef ojeda de Oliveira <alef.oliveira@siedos.com.br>
2025-11-30 22:21:22 -0300

    refactor: conditionally load dotenv-rails only in development and test environments

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-require "dotenv-rails"
+require "dotenv-rails" if Rails.env.development? || Rails.env.test?
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
