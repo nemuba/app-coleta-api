50552025d54c245ac01fc5bb4908b4bdb4f04f28
Alef ojeda de Oliveira <alef.oliveira@siedos.com.br>
2025-12-09 22:18:16 -0300

    config: disable ActiveRecord SQLite3 production warning

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,7 @@ module CollectApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_record.sqlite3_production_warning = false
   end
 end
